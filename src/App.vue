168b9d5c335be71518cd8ba69f58fa0fec761557
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-07-03 21:44:24 +0200

    feat: implement Button component and integrate clipboard functionality in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Button from './components/ui/button/Button.vue';
+import {useClipboard} from '@vueuse/core'
+const clipboard = useClipboard();
+
+function copyToClipboard() {
+  if(clipboard.isSupported) {
+    clipboard.copy('Hello World!')
+  } else { 
+    alert('Clipboard copy is not supported by this browser')
+  }
+}
 </script>
 
 <template>
   <RouterView />
-
-  <Button >Copy to clipboard</Button>
+  <Button @click="copyToClipboard">Copy to clipboard</Button>
 </template>
 
 <style scoped>
