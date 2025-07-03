0471341ee264a7708e69dfda2a417303181c2b0e
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-07-03 21:30:46 +0200

    feat: update dependencies and enhance App.vue with a new Button component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Button from './components/ui/button/Button.vue';
 </script>
 
 <template>
   <RouterView />
+
+  <Button >Copy to clipboard</Button>
 </template>
 
 <style scoped>
