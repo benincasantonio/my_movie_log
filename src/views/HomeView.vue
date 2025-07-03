9c8b96f3f18deafa83f0f2d9b495349737f0ee1f
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-07-03 21:14:19 +0200

    remove about route and related components; simplify home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to my movie log</h1>
   </main>
 </template>
